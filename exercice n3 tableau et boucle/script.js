d26e0ba449c67fdaee531d9787aacb3fdcd3eca2
n0wii <145431035+n0wii@users.noreply.github.com>
2023-09-22 12:17:16 +0200

    Add files via upload

---
+++ exercice n3 tableau et boucle/script.js
@@ -0,0 +1,16 @@
+//  Boucle
+
+let tableauFruit=4;
+for (let i=0;i<tableauFruit;i++){
+    console.log('OKLM');
+}
+
+//  Tableau
+
+let names=[];
+names.push('Vincent',"Paul",'Arthur');
+
+names.forEach(name =>{
+    name += 'va a la peche ';
+    console.log(name)
+})
